4f8fc9b4fc9aadcef16059125788bfbe0e28a0cb
Kevin Ramnauth <Kevin2107@gmail.com>
2017-07-16 14:24:18 -0400

    lesson 4

--- Project Data/decision_tree/dt_author_id.py
+++ Project Data/decision_tree/dt_author_id.py
@@ -23,8 +23,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier()
+
+#features_train = features_train[:len(features_train[0])] 
+
+
+#print features_train.shape
+
+#Train
+t0 = time()
+clf.fit(features_train, labels_train)
+print ("training time:", round(time()-t0, 3), "s")
+#predict
+t0 = time()
+pred = clf.predict(features_test)
+print ("predicting time:", round(time()-t0, 3), "s")
+
+accuracy = accuracy_score(pred, labels_test)
+print ('accuracy = {0}'.format(accuracy))
 
 #########################################################
 
